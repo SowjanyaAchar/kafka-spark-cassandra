8967e00fef055fcd3cab0d6ab5bd4f9be24c8dfd
SowjanyaAchar <55071276+SowjanyaAchar@users.noreply.github.com>
2019-12-05 01:07:48 -0700

    Update and rename kafka-cassandra.sh to kafka-spark-cassandra.sh

--- kafka-spark-cassandra.sh
+++ kafka-spark-cassandra.sh
@@ -3,9 +3,10 @@
 
 git clone https://github.com/shsh9888/kafka-on-kubernetes
 git clone https://github.com/shsh9888/cassandra-on-kubernetes.git
+git clone https://github.com/SowjanyaAchar/spark-on-kubernetes
 
 mkdir -p build
 cp kafka-on-kubernetes/*.yaml ./build/
 cp cassandra-on-kubernetes/*.yaml ./build/
-
+cp spark-on-kubernetes/*.yaml ./build/
 cd build && kubectl create -f .
